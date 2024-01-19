cb2ed9c52a2854f8ea9145c7bce0f57bc0fd9929
Andrus <xwen0110@gmail.com>
2024-01-19 02:45:24 +0100

    dashboard/section5

--- src/components/dashboard/index.tsx
+++ src/components/dashboard/index.tsx
@@ -1,11 +1,13 @@
 import { BestServiceSection } from "./section3";
 import FeaturedPostSection from "./section4";
+import TestimonialSection from "./section5";
 
 export default function Dashboard() {
   return (
     <>
       <BestServiceSection />
       <FeaturedPostSection />
+      <TestimonialSection />
     </>
   );
 }
---
+++ src/components/dashboard/section5/index.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import { Box, Container, Typography } from "@mui/material";
+import StarIcon from "@/assets/svgs/star.svg";
+import OutlinedStarIcon from "@/assets/svgs/outlinedStar.svg";
+import UserImage from "@/assets/images/users/user.png";
+import TestimonialImage1 from "@/assets/images/testimonials/testimonials-1.png";
+import TestimonialImage2 from "@/assets/images/testimonials/testimonials-2.png";
+import TestimonialImage3 from "@/assets/images/testimonials/testimonials-3.png";
+import TestimonialImage4 from "@/assets/images/testimonials/testimonials-4.png";
+import TestimonialImage5 from "@/assets/images/testimonials/testimonials-5.png";
+import TestimonialImage6 from "@/assets/images/testimonials/testimonials-6.png";
+import TestimonialImage7 from "@/assets/images/testimonials/testimonials-7.png";
+import TestimonialImage8 from "@/assets/images/testimonials/testimonials-8.png";
+import TestimonialImage9 from "@/assets/images/testimonials/testimonials-9.png";
+import Image from "next/image";
+
+const imageSource = [
+  TestimonialImage1.src,
+  TestimonialImage2.src,
+  TestimonialImage3.src,
+  TestimonialImage4.src,
+  TestimonialImage5.src,
+  TestimonialImage6.src,
+  TestimonialImage7.src,
+  TestimonialImage8.src,
+  TestimonialImage9.src,
+];
+
+export default function TestimonialSection() {
+  return (
+    <Container maxWidth="lg" sx={{ p: { xs: 3, lg: 4.5 } }}>
+      <Box
+        display="flex"
+        flexDirection={{ xs: "column", md: "row" }}
+        justifyContent="space-between"
+        p={{ xs: 0, lg: 6 }}
+      >
+        <Box display="flex" flexDirection="column" gap="28px">
+          <Typography
+            variant="h3"
+            sx={{ paddingTop: "18px", textAlign: "center" }}
+          >
+            What they say about us
+          </Typography>
+          <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            gap="20px"
+            p={{ xs: 6, md: "30px 35px" }}
+          >
+            <Box
+              sx={{
+                height: "90px",
+                width: "90px",
+                border: "1.8px solid",
+                borderColor: "text.muted",
+                borderRadius: "90px",
+                background: `url(${UserImage.src}) lightgray 50% / cover no-repeat`,
+              }}
+            />
+            <Box gap="5px">
+              <StarIcon />
+              <StarIcon />
+              <StarIcon />
+              <StarIcon />
+              <OutlinedStarIcon />
+            </Box>
+            <Typography
+              variant="h6"
+              sx={{ textAlign: "center", color: "text.secondary" }}
+            >
+              {`Slate helps you see how many more days you need to work to reach your financial goal.`}
+            </Typography>
+            <Box display="flex" flexDirection="column">
+              <Typography
+                variant="body1"
+                sx={{ fontWeight: 700, color: "primary.main" }}
+              >{`Regina Miles`}</Typography>
+              <Typography
+                variant="body1"
+                sx={{ fontWeight: 700 }}
+              >{`Designer`}</Typography>
+            </Box>
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            display: "grid",
+            gridTemplateColumns: "repeat(3, minmax(0, 1fr))",
+            gap: "17px 16px",
+          }}
+        >
+          {imageSource.map((imgPath) => (
+            <Box
+              key={`testimonial-${imgPath}`}
+              position="relative"
+              width="100%"
+              height={{ xs: "85px", md: "143px" }}
+              sx={{
+                aspectRatio: "1/1",
+              }}
+            >
+              <Image alt={`testimonial-image`} src={imgPath} fill />
+            </Box>
+          ))}
+        </Box>
+      </Box>
+    </Container>
+  );
+}
